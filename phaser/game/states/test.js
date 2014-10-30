cb1e6b7bed56a4437015a88e8384d2b8e082d772
mf <mf@tt4.at>
2014-10-30 19:48:21 +0100

    whitespace, semicolon

--- phaser/game/states/test.js
+++ phaser/game/states/test.js
@@ -3,7 +3,7 @@
 var Tiled = require('../model/tiled.js');
 var WorldBlocks = require('../model/world');
 var frp = require('../frp/behavior.js');
-var b = require('../frp/behavior2.js')
+var b = require('../frp/behavior2.js');
 
 function TestState() {}
 
@@ -17,7 +17,7 @@ TestState.prototype = {
         //var e = Tiled.MakeObjectLayerSprites(this.game, this.map, 'Object Layer 1');
         //console.log(e);
         this.moveEvent = new frp.EventStream();
-      
+
         this.frpWorld = new b.World(this.game);
         this.game.world.width = 20000;
         this.game.world.height = 5000;
@@ -31,7 +31,7 @@ TestState.prototype = {
         var playerSprite = this.frpPlayer.sprite;
         this.player.sprite = playerSprite;
         this.game.tiled_loader.runInterpreter(new Tiled.BaseInterpreter(this.frpWorld));
-      
+
         var background3 = this.game.add.sprite (0, 0, 'background3');
         var background2 = this.game.add.sprite (0, this.game.height - 116 - 100, 'background2');
         var background1 = this.game.add.sprite (0, this.game.height - 70 - 100, 'background1');
@@ -62,9 +62,9 @@ TestState.prototype = {
       var that = this;
 
       this.frpPlayer.setPosition(new b.Direction (this.player.sprite.body.x, this.player.sprite.body.y));
-      
+
       var keyboard = this.game.input.keyboard;
-      
+
       if (keyboard.isDown(Phaser.Keyboard.P)) {
           console.log("Player", this.frpPlayer);
       }
@@ -79,35 +79,35 @@ TestState.prototype = {
       if (this.game.input.keyboard.isDown(Phaser.Keyboard.S)) {
           this.frpPlayer.setBlockEvent.send(true);
       }
-      
+
       if (this.moving === false && keyboard.isDown(Phaser.Keyboard.A) && !keyboard.isDown(Phaser.Keyboard.D)) {
           this.frpPlayer.moveEvent.send(new b.MoveEvent(-1, 0));
           this.moving = true;
-      } 
+      }
       if (this.moving === false && !keyboard.isDown(Phaser.Keyboard.A) && keyboard.isDown(Phaser.Keyboard.D)) {
           this.frpPlayer.moveEvent.send(new b.MoveEvent(1, 0));
           this.moving = true;
-      } 
+      }
 
       if (!keyboard.isDown(Phaser.Keyboard.A) && !keyboard.isDown(Phaser.Keyboard.D)) {
           this.moving = false;
           this.frpPlayer.moveEvent.send(new b.StopMoveEvent());
       }
-      
+
       if (this.qDown === false && keyboard.isDown(Phaser.Keyboard.Q)) {
           this.frpPlayer.setMovementSystem.send('BaseMovement')
           this.qDown = true;
       } else if (this.qDown === true && !keyboard.isDown(Phaser.Keyboard.Q)) {
           this.qDown = false;
       }
-      
+
       if (this.eDown === false && keyboard.isDown(Phaser.Keyboard.E)) {
           this.frpPlayer.setMovementSystem.send('OtherMovement')
           this.eDown = true;
       } else if (this.qDown === true && !keyboard.isDown(Phaser.Keyboard.E)) {
           this.eDown = false;
       }
-      
+
       if (keyboard.isDown(Phaser.Keyboard.F)) {
           this.frpWorld.camera.shakeMe.send(true);
       }
@@ -118,7 +118,7 @@ TestState.prototype = {
       b.tick.send(this.game.time.elapsed);
       // update tick
       frp.system.sync();
-      
+
       //
       // Test for pause key
       //
